7f62ddfaadb3917be9d0699a726087a92ba211fb
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-05-27 18:03:20 -0300

    CArgo modulo para minificar css en gulp y primer task

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,5 +1,9 @@
-let gulp = require('gulp');
+const { src, dest } = require('gulp');
+const minifycss = require('gulp-minify-css');
 
-gulp.task('construct', () => {
-    console.log('hola')
-})
+exports.default = () => {
+    console.log('Start minify css..');
+    return src('./assets/css/*.css')
+        .pipe(minifycss())
+        .pipe(dest('./dist/css/'));
+}
