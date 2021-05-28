97d5007d2e9c43e362eeec29bd6ccdd1697540d5
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-05-28 15:12:01 -0300

    Corrijo ruta de import

--- frontend/assets/js/project.js
+++ frontend/assets/js/project.js
@@ -1,4 +1,4 @@
-import '../../node_modules/swiper/swiper-bundle.js';
+import '../../node_modules/swiper/swiper-bundle.min.js';
 import '../../node_modules/glightbox/dist/js/glightbox.min.js';
 import setImageObjectFit from './utilities/logicObjetcFit.js';
 
