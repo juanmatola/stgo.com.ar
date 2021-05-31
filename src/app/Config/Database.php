1ad2d4df98525671cf810f70191b3832e4eb602c
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-05-31 11:12:11 -0300

    Cargo primer diseño de db

--- src/app/Config/Database.php
+++ src/app/Config/Database.php
@@ -32,9 +32,9 @@ class Database extends \CodeIgniter\Database\Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => '',
+		'username' => 'root',
 		'password' => '',
-		'database' => '',
+		'database' => 'stgo_db',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
