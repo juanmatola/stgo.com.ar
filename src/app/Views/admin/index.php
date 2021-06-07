4bc1abe680f388726ca4175a0de8b41e1f1b67ba
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-06-07 16:28:28 -0300

    Corrijo estilos en panel de admin

--- src/app/Views/admin/index.php
+++ src/app/Views/admin/index.php
@@ -1,6 +1,6 @@
 <div class="row">
-    <div class="col-md-6 col-12 text-left"><h1>Proyectos</h1></div>
-    <div class="col-md-6 col-12 text-center text-md-right my-2"><button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addModal">+ Nuevo</button></div>
+    <div class="col-md-6 col-12 text-start"><h1>Proyectos</h1></div>
+    <div class="col-md-6 col-12 text-end my-2"><button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addModal">+ Nuevo</button></div>
 </div>
 
 <table class="table table-hover">
@@ -25,7 +25,6 @@
       <td><?php echo $post['team']; ?></td>
       <td class="mobile-none"><?php echo $post['description']; ?></td>
       <td>
-        <!-- <a class="btn btn-danger" href="<?php echo base_url().'/admin/deletepost?id='.$post['id']; ?>" ><i class="far fa-trash-alt"></i></a> -->
         <button type="button" class="btn btn-danger my-2" data-bs-toggle="modal" data-bs-target="#deleteModal" onclick="deleteModalOpen('<?php echo $post['id']; ?>')">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill:rgba(255, 255, 255, 1);transform:;-ms-filter:"><path fill="none" d="M17.004 20L17.003 8h-1-8-1v12H17.004zM13.003 10h2v8h-2V10zM9.003 10h2v8h-2V10zM9.003 4H15.003V6H9.003z"></path><path d="M5.003,20c0,1.103,0.897,2,2,2h10c1.103,0,2-0.897,2-2V8h2V6h-3h-1V4c0-1.103-0.897-2-2-2h-6c-1.103,0-2,0.897-2,2v2h-1h-3 v2h2V20z M9.003,4h6v2h-6V4z M8.003,8h8h1l0.001,12H7.003V8H8.003z"></path><path d="M9.003 10H11.003V18H9.003zM13.003 10H15.003V18H13.003z"></path></svg>
           </button>
