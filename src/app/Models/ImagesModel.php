3920b76244505eff3275f5b5b3a2a2d6c8f9e8c5
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-06-07 14:33:25 -0300

    Diseño modelo de imagenes

---
+++ src/app/Models/ImagesModel.php
@@ -0,0 +1,17 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ImagesModel extends Model
+{
+    protected $table = 'is_image';
+    protected $primaryKey = 'id';
+    protected $returnType =  'array';
+    protected $allowedFields = ['post_id','url'];
+
+    public function getByPostId($post_id)
+    {
+        return $this->where('post_id', $post_id)
+                    ->findAll();
+    }
+}
