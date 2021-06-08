ecd725ccc4dd4f9a8f71f5225f255b1bdae738fc
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-06-08 12:18:40 -0300

    Comienzo de logica de hader/footer

--- src/app/Views/templates/footer.php
+++ src/app/Views/templates/footer.php
@@ -1,3 +1,6 @@
         <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        <?php if($page == 'simple-project'): ?>
+            <script src="./assets/js/project.js" type="module"></script>  
+        <?php endif; ?> 
 </body>
 </html>
--- src/app/Views/templates/header.php
+++ src/app/Views/templates/header.php
@@ -6,13 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | STGO estudio de arquitectura | Rosario, Santa Fe, Argentina</title>
     <meta name="description" content="Description">
-    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="<?= base_url() ?>/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?= base_url() ?>/favicon.ico" type="image/x-icon">
+
+    <!-- Og metas -->
+    <meta property="og:url"                content="<?= base_url() ?>" />
+    <meta property="og:type"               content="article" />
+    <meta property="og:title"              content="Proyectos | STGO estudio de arquitectura | Rosario, Santa Fe, Argentina"/>
+    <meta property="og:description"        content="Description" />
+    <meta property="og:image"              content="<?= base_url() ?>/assets/img/ogimage.jpg" />
+
     <!-- Css -->
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="stylesheet" href="./assets/css/index-animation.css">
-    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
-     <!-- Fontawesome kit -->
-     <script src="https://kit.fontawesome.com/4657d46491.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/style.css">                                              
+    <link rel="stylesheet" href="<?= base_url() ?>/node_modules/bootstrap/dist/css/bootstrap.min.css">                 
+
+<?php if($page == 'index'): ?>
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/index-animation.css">                                   
+<?php endif; ?>
+<?php if($page == 'about'): ?>
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/about.css">                                              
+<?php endif; ?>
+<?php if($page == 'project' || $page == 'simple-project'): ?>
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/projecs.css">                                            
+<?php endif; ?> 
+<?php if($page == 'simple-project'): ?>
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/simple-project.css">                                      
+    <!-- Carousel Css -->   
+    <link rel="stylesheet" href="<?= base_url() ?>/node_modules/swiper/swiper-bundle.min.css"/>                         
+    <!-- Light Box Css -->
+    <link rel="stylesheet" href="<?= base_url() ?>/node_modules/glightbox/dist/css/glightbox.min.css">                     
+<?php endif; ?> 
+    <!-- Fontawesome kit -->
+    <script src="https://kit.fontawesome.com/4657d46491.js" crossorigin="anonymous"></script>           
 </head>
 <body>
