dfc1c712519abec02779a814058cacf10ef4dbbb
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-06-11 14:33:54 -0300

    Listo muestra de proyectos en vista

--- src/app/Views/projects.php
+++ src/app/Views/projects.php
@@ -4,88 +4,17 @@
 
     <div class="row projects">
 
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
+    <?php foreach ($projects as $project): ?>
 
       <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
-
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
-
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
-
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
-
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
-
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
-
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
-
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
-        </a>
-      </div>
-
-      <div class="col-12 col-md-4 project-container">
-        <img src="./assets/img/home.jpg" alt="project image" class="project-container_image">
-        <a class="project-container_wrapper" href="./simple-project.html">
-          <h2>Lorem</h2>
+        <img src="<?= base_url()?>/writable/uploads/portfolio/<?= $project['url']; ?>" alt="project image" class="project-container_image">
+        <a class="project-container_wrapper" href="<?= base_url()?>/proyectos/<?= $project['id'];?>">
+          <h2><?= $project['title']; ?></h2>
         </a>
       </div>
       
+    <?php endforeach; ?>
+
     </div>
 
   </main>
