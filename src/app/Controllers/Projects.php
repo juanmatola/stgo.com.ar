d172ec811a99aa4fc75457e59a072687e169be52
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-06-11 14:22:59 -0300

    Preparo info de ProjectsPreviews para enviar a vista

--- src/app/Controllers/Projects.php
+++ src/app/Controllers/Projects.php
@@ -1,16 +1,32 @@
 <?php namespace App\Controllers;
 
+use App\Models\PostsModel;
+use App\Models\ImagesModel;
+
 class Projects extends BaseController
 {
 	public function index()
 	{
+		$postmodel = new PostsModel();
+		$imagemodel = new ImagesModel();
+
+		$previews = $postmodel->getPreviews();
+
+		foreach ($previews as $key => $project ) {
+			$img = $imagemodel->getPreview($project['id']);
+            $previews[$key] = $previews[$key] + $img;
+        }
 		
 		$data = [
 			'page' => 'projects',
 		];
+		$projects_data = [
+			'projects' => $previews, 
+		];
+
 
 		echo view('templates/header', $data);
-		echo view('projects');
+		echo view('projects', $projects_data);
 		echo view('templates/footer', $data);
 	}
 
