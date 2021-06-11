dff22e6291e945bf0640481aa755938c802dddf8
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-06-11 14:22:30 -0300

    Creo metodo en ImageModel para obtener primer imagen de proyecto segun id

--- src/app/Models/ImagesModel.php
+++ src/app/Models/ImagesModel.php
@@ -9,6 +9,18 @@ class ImagesModel extends Model
     protected $returnType =  'array';
     protected $allowedFields = ['post_id','url'];
 
+    public function getPreview($post_id)
+    {
+        $imageArray = $this->where('post_id', $post_id)->select('url')->selectMin('id')->find();
+        try {
+            unset($imageArray[0]['id']);
+            $imageArray = $imageArray[0];
+            return $imageArray;
+        } catch (\Throwable $th) {
+            return false;
+        }  
+    }
+
     public function getByPostId($post_id)
     {
         return $this->where('post_id', $post_id)
